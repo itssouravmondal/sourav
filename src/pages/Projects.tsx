ca8968f84b42e4d35dbe82a9ad640257427a7145
Sourav Mondal <96481691+itssouravmondal@users.noreply.github.com>
2025-03-30 15:48:07 +0530

    Update Projects.tsx

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -6,7 +6,7 @@ function Projects() {
       title: "Interview Insights: ResumePro ",
       description: "An AI-powered tool designed to revolutionize interview preparation by automatically generating personalized interview questions based on a candidate’s resume. Leveraging advanced resume analysis, ResumePro Interview Insights creates contextually relevant questions tailored to a candidate’s experience, skills, and cultural fit. This tool streamlines the hiring process, ensuring consistency, efficiency, and deeper candidate assessments while reducing the time recruiters spend on manual question creation.",
       links: {
-        web: "https://example.com/project1",
+        web: "https://docs.google.com/document/d/e/2PACX-1vTFBgrJLsE_KqqRTZJXZ2eohrtpWUeBSq0fcxGUyjuvgc1ay_jv8mgkjYk40d5w2VN8JJ-uXbK2xG7q/pub",
         doc: "https://docs.google.com/document/d/e/2PACX-1vQp14igr-UqvlgiHyskxnOrcQCu0UrZtIEsPjEYuuGc8QJ4qufREYhB7WJ6mpGKAQ/pub",
         ppt: "https://docs.google.com/presentation/d/e/2PACX-1vRJUEKu2Agrz_tIMCUUgvpJL-WvlzR8ldDHtRhMAAWOzhSm48sjSIjb1YgifOlYxw/pub?start=true&loop=true&delayms=3000"
       }
@@ -15,7 +15,7 @@ function Projects() {
       title: "Image Generator: VectorGen Spectrum",
       description: "An AI-powered image generation tool built on VectorShift’s Generative AI technology, enabling users to create high-quality, realistic, and artistic images from text, blogs, and articles. Designed for seamless content enhancement, VectorGen Spectrum transforms written concepts into visually compelling visuals, streamlining creative workflows for designers, marketers, and content creators.",
       links: {
-        web: "https://example.com/project2",
+        web: "https://docs.google.com/document/d/e/2PACX-1vTFBgrJLsE_KqqRTZJXZ2eohrtpWUeBSq0fcxGUyjuvgc1ay_jv8mgkjYk40d5w2VN8JJ-uXbK2xG7q/pub",
         doc: "https://docs.google.com/document/d/e/2PACX-1vRie7d21CnXD-KpHiCPTy7nA7465zxjQIwiA_rcH5L2PXPQPnu128AFypV8s0k_eQ/pub",
         ppt: "https://docs.google.com/presentation/d/e/2PACX-1vR7pJjP_PrhoQNKEazoP4pfdv5uChTU68zxCCGYfpb57p3ELhtP8tcBjNhjplPYUw/pub?start=true&loop=true&delayms=3000"
       }
@@ -24,7 +24,7 @@ function Projects() {
       title: "GenAI Powered Customer Support Chatbot",
       description: "An intelligent AI assistant designed to provide instant, accurate guidance on music metadata best practices, service offerings, and licensing agreements. By streamlining information delivery and reducing support ticket volume, the chatbot will enhance user experience while ensuring consistency in responses.",
       links: {
-        web: "https://example.com/project3",
+        web: "https://docs.google.com/document/d/e/2PACX-1vTFBgrJLsE_KqqRTZJXZ2eohrtpWUeBSq0fcxGUyjuvgc1ay_jv8mgkjYk40d5w2VN8JJ-uXbK2xG7q/pub",
         doc: "https://docs.google.com/document/d/e/2PACX-1vR4Cp2Ks2PsYIZX7ObmdttO0bWpsOTnDa-iVHKglxXvyxAnVZB8uq9lSEHiLI_TZA/pub",
         ppt: "https://docs.google.com/presentation/d/e/2PACX-1vSQMf-cdUOMuuxTounh3Ppuo_5RrZ0haX98u5UX4IK3RyG1PSigbn099oEqcmZ0gg/pub?start=true&loop=true&delayms=3000"
       }

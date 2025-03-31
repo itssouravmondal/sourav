f142b9f43aa5849d48576b75c114e2b2b63cc7b2
Sourav Mondal <96481691+itssouravmondal@users.noreply.github.com>
2025-03-31 07:21:22 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -90,7 +90,7 @@ function Home() {
     href="https://www.linkedin.com/in/itssouravmondal/" 
     target="_blank" 
     rel="noopener noreferrer" 
-    className="p-2 rounded-full hover:bg-opacity-20 hover:backdrop-blur-sm"
+    className="p-2 rounded-full bg-gray-100 hover:bg-gray-200 transition duration-300"
   >
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-linkedin">
       <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-4 0v7h-4v-7a6 6 0 0 1 6-6z"></path>
